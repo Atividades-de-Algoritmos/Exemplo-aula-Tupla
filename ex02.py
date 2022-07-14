5048925c9fb189850c6e464ca0eeb204cd914f55
Carlos <lopes.carlos@academico.ifpb.edu.br>
2022-07-14 00:07:32 -0300

    Verificando Valores ✅

--- ex02.py
+++ ex02.py
@@ -4,65 +4,62 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 28/06/2022
+# data: 14/07/2022
 #
-# 2 – verifique se um determinado elemento esta
+# 2 – Verifique se um determinado elemento esta
 # contido na tupla ('a','b',1,2,'c',3,4,'d',5)
-#
-# criana uma tupla com valores ('a','b',1,2,'c',3,4,'d',5)
-t1 = ('a', 'b', 1, 2, 'c', 3, 4, 'd', 5)
 
-# imprimir os elementos da tupla
-for i in t1:
-    print(i)  # imprime os elementos da tupla
+t1 = ('a', 'b', 1, 2, 'c', 3, 4, 'd', 5) # Criando uma tupla com valores ('a','b',1,2,'c',3,4,'d',5)
 
-# imprimir a tupla
-for i in t1:
-    print(t1)  # imprime a tupla varias vezes
+print(f'Tupla: {t1}\n') # Imprimindo a tupla no terminal
 
-# verificar se o elemento 'b' esta contido na tupla
-print('b' in t1)  # imprime True
+# Verificando alguns valores
+print('"e" na tupla ->', 'e' in t1) # False (não contido)
+print('"f" na tupla ->', 'f' in t1) # False (não contido)
+print('"g" na tupla ->', 'g' in t1) # False (não contido)
 
-print('a' in t1) # True (contido)
-print('e' in t1) # False (não contido)
-print('e' not in t1) # True (não contido)
+for i in t1: # Para cada item armazenado na tupla
+  print(f'"{i}" na tupla -> {i in t1}') # Imprime True or False para o item do ciclo atual
 
-for i in range(len(t1)):
-  print(i in t1) # imprime True para cada indice contido na tupla
+var = input('\nDigite um valor pra ver se tem na tupla: ') # Solicitando valor do usuário
 
-for i in range(len(t1)):
-  print(f"{i} = {i in t1}") # imprime True para cada indice contido na tupla
+print(f'\n"{var}" na tupla ->', {var} in t1) # Verificando se existe ou não
 
+# -- [ Python enumerate() ] --
 
-#############################################################################
-print("--------------------------------------------------------------------")
-#############################################################################
-# Python enumerate()
-# O enumerate()método adiciona um contador a um iterável e o retorna (o objeto enumerate).
-# O enumerate() é um objeto que representa um contador.
-# exemplo:
-# linguagens = ['Python', 'Java', 'JavaScript']
-#
-# enumerate_prime = enumerate(linguagens)
-#
-# # converte o objeto enumerate_prime em uma lista
-# print(list(enumerate_prime))
-#
-# # saida: [(0, 'Python'), (1, 'Java'), (2, 'JavaScript')]
+# - O enumerate() é um método que quando chamado representa uma tupla sendo o primeiro valor o índice e o segundo o valor real.
+# - Exemplo (Único parâmetro):
+
+# 01 - linguagens = ['Python', 'Java', 'JavaScript']
+
+# 02 - enumerate_prime = enumerate(linguagens)
+
+# 03 - print(list(enumerate_prime)) # Convertendo o objeto em uma lista
+
+# Return: [(0, 'Python'), (1, 'Java'), (2, 'JavaScript')]
 
 
-# Sintaxe de enumerar():
-# enumerate(iterável, start=0)
-# enumerate() Parâmetros:
-# O método recebe dois parâmetros:
-# # iterável - uma sequência, um iterador ou objetos que suportam iteração
-# start (opcional) - enumerate()inicia a contagem a partir deste número. Se start for omitido,
-# 0 é tomado como start.
+# -- [ Parâmetros do método enumerate() ] --
 
-t2 = enumerate(t1) # cria um enumerate para a tupla t1
-print(t2) # imprime o enumerate da tupla t1 (indice, elemento)
-print(list(t2)) # converte o enumerate em lista (indice, elemento)
+# - Sintax: enumerate(iterável, start=0)
 
-# loop para imprimir o enumerate da tupla t1 (indice, elemento)
-for i, valor in enumerate(t1): # enumerate retorna um iterador de pares (indice, valor) da tupla
-   print(f"indice[{i}] -> {valor}") # imprime o indice e o valor do elemento da tupla
+# O método recebe dois parâmetros;
+# iterável(Primeiro) - Todo objeto que permite que um loop seja executado nele.
+# start (opcional) - Quando o start não é informado sub-entende que é 0, quando é informado a iteração começa do que foi chamado.
+
+# Exemplo (Dois parâmetros):
+
+# 01 - linguagens = ['Python', 'Java', 'JavaScript']
+
+# 02 - enumerate_prime = enumerate(linguagens, start = 5)
+
+# 03 - print(list(enumerate_prime)) # Convertendo o objeto em uma lista
+
+# Return: [(5, 'Python'), (6, 'Java'), (7, 'JavaScript')]
+
+"""t2 = enumerate(t1) # Criando um enumerate para a tupla t1
+print(t2) # Imprimindo o objeto do enumerate da tupla t1
+print(list(t2)) # Convertendo o enumerate em lista (indice, elemento)
+"""
+
+print('\nfim do programa') # Informando ao usuário que o programa terminou
